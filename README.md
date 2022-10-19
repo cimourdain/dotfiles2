2f30ce6c25e8f0da150899c44cb77e9df2f05a61
Gabriel Oger <gabriel.oger@gmail.com>
2022-10-19 18:04:55 +0200

    :tada:init repo

---
+++ README.md
@@ -0,0 +1,36 @@
+# Backup
+```bash
+./script.sh backup
+```
+
+Then save securely the content of the `private/` folder
+
+# Restore
+## Pre-requisite
+Restore manually the content of the `private/` folder
+
+
+# Run
+```bash
+./script.sh restore
+```
+
+# Init
+After restore init applications
+```bash
+./script.sh init
+```
+
+
+# MA
+
+## Wifi
+Dashlane: Wifi-MA-GEEK5
+
+## VPN
+load from vpn/ Password w/ TLS
+
+
+## TODO
+ - check ssh keys & GPG private key exists
+ - systemd service for auto backup
---
+++ script.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+
+# source .env folder
+set -a; source .env; set +a
+
+
+################
+# GPG
+################
+function backup_gpg {
+    echo "Backup gpg keys"
+    rm -rf ${BACKUP_FOLDER}gpg
+    mkdir -p ${BACKUP_FOLDER}gpg
+    gpg --export --export-options backup --output "${BACKUP_FOLDER}gpg/public.gpg"
+    gpg --export-secret-keys --export-options backup --output "${BACKUP_FOLDER}gpg/private.gpg"
+    gpg --export-ownertrust > "${BACKUP_FOLDER}gpg/trust.gpg"
+}
+
+function restore_gpg {
+    gpg_files_path="${BACKUP_FOLDER}gpg/"
+    if [[ -f ${gpg_files_path}public.gpg && -f ${gpg_files_path}private.gpg ]]; then
+        echo "restore GPG keys"
+        gpg --import "${BACKUP_FOLDER}gpg/public.gpg"
+        gpg --import "${BACKUP_FOLDER}gpg/private.gpg"
+        if [[-f ${gpg_files_path}trust.gpg  ]];then
+            gpg --import-ownertrust "${BACKUP_FOLDER}gpg/trust.gpg"
+        else
+            echo "WARNING: no gpg trust file found in ${gpg_files_path}"
+        fi
+    else
+        echo "ERROR: No key files found in ${gpg_files_path}"
+    fi
+    
+}
+
+
+################
+# SSH
+################
+function backup_ssh {
+  echo "Backup ssh keys"
+  rm -rf ${BACKUP_FOLDER}ssh
+  mkdir -p ${BACKUP_FOLDER}ssh
+  cp ${HOME}/.ssh/id_rsa* ${BACKUP_FOLDER}ssh
+  cp ${HOME}/.ssh/config ${BACKUP_FOLDER}ssh
+}
+
+function restore_ssh {
+    ssh_files_path="${BACKUP_FOLDER}ssh/"
+    ssh_folder=${HOME}/.ssh
+    cp -r ${ssh_files_path}* ${ssh_folder}
+    chmod 700 ${ssh_folder}
+    chmod 644 ${ssh_folder}/config
+    chmod 600 ${ssh_folder}/id_*
+    chmod 644 ${ssh_folder}/*.pub
+}
+
+################
+# Bashrc
+################
+function restore_bashrc {
+
+    if [[ ! -L "$HOME/.bashrc" ]]; then
+        echo "Save current .bashrc to $HOME/.bashrc.bak"
+        mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
+    else
+        rm "$HOME/.bashrc"
+    fi
+    
+    echo "create bashrc symlink"
+    ln -s ${PWD}/bashrc/.bashrc ${HOME}
+}
+
+################
+# GIT Config
+################
+function restore_git {
+    if [[ -f ${HOME}/.gitignore_global ]];then
+        if [[ ! -L ${HOME}/.gitignore_global ]];then
+            mv ${HOME}/.gitignore_global ${HOME}/.gitignore_global.bak
+        fi
+    fi
+
+    if [[ -f ${HOME}/.gitconfig ]];then
+        if [[ ! -L ${HOME}/.gitconfig ]];then
+            mv ${HOME}/.gitconfig ${HOME}/.gitconfig.bak
+        fi
+    fi
+
+    echo "git config symlinks"
+    ln -s ${PWD}/gitfiles/.gitignore_global ${HOME}
+    ln -s ${PWD}/gitfiles/.gitconfig ${HOME}
+}
+
+################
+# Docker
+################
+function restore_docker {
+    echo "enable docker to current user"
+    # Create the docker group if it does not exist
+    sudo groupadd docker
+    # Add your user to the docker group.
+    sudo usermod -aG docker $USER
+    # Log in to the new docker group (to avoid having to log out / log in again)
+    newgrp docker
+}
+
+
+################
+# SYSTEM
+################
+function restore_system {
+    echo "update arch mirror list"
+    sudo reflector --save /etc/pacman.d/mirrorlist --country France,Germany,Finland,Netherland --protocol https --latest 10
+
+    echo "restore installed packages"
+    pacman -Syyu
+    pacman --noconfirm -Sy $(grep -Ev ^'(#|$)' "${PWD}/pacman.txt")
+}
+
+
+################
+# SYSTEMD
+################
+function restore_startup_services {
+    echo "enable services"
+    sudo systemctl enable bluetooth.service
+    sudo systemctl enable docker.service
+}
+
+################
+# GCLOUD
+################
+function init_gcloud {
+    gcloud init
+    gcloud auth application-default login
+    gcloud auth configure-docker
+}
+
+
+################
+# MAIN
+################
+function backup {
+  backup_gpg
+  backup_ssh
+}
+
+function restore {
+    restore_system
+    restore_bashrc
+    restore_git
+    restore_gpg
+    restore_ssh
+}
+
+
+function init {
+    init_gcloud
+}
+
+# Check if the function exists (bash specific)
+if declare -f "$1" > /dev/null
+then
+  # call arguments verbatim
+  "$@"
+else
+  # Show a helpful error
+  echo "'$1' is not a known function name" >&2
+  exit 1
+fi
