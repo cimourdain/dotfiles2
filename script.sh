0a494aac9e0f7c42452e478237e27dfa01a78b40
Gabriel Oger <gabriel.oger@gmail.com>
2024-06-13 10:24:42 +0200

    feat: vscode sync extensions

--- script.sh
+++ script.sh
@@ -30,7 +30,7 @@ function restore_gpg {
     else
         echo "ERROR: No key files found in ${gpg_files_path}"
     fi
-    
+
 }
 
 
@@ -60,14 +60,14 @@ function restore_ssh {
 # Bashrc
 ################
 function restore_bashrc {
-    
+
     if [[ ! -L "$HOME/.bashrc" ]]; then
         echo "Save current .bashrc to $HOME/.bashrc.bak"
         mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
     else
         rm "$HOME/.bashrc"
     fi
-    
+
     echo "create bashrc symlink"
     ln -s ${PWD}/bashrc/.bashrc ${HOME}
 }
@@ -101,19 +101,73 @@ function restore_bashrc {
 # }
 
 ################
-# Bashrc
+# VsCode
 ################
-function restore_vscode {
-    mkdir -p $HOME/.config/Code/User
-    if [[ ! -L "$HOME/.config/Code/User/settings.json" ]]; then
-        echo "Save current ${HOME}/.config/Code/User/settings.json to ${HOME}/.config/Code/User/settings.json.back"
-        mv "${HOME}/.config/Code/User/settings.json" "${HOME}/.config/Code/User/settings.json.back"
-    else
-        rm "${HOME}/.config/Code/User/settings.json"
+function createSymlink {
+    local target_folder=$1
+    local target_filename=$2
+    local source_filepath=$3
+
+    local target_path="${target_folder}/${target_filename}"
+
+    echo "# Create folder (if not exists): ${target_folder}"
+    mkdir -p ${target_folder}
+
+    if [[ -f "${target_path}" ]]; then
+        echo "${target_path} already exists"
+
+        if [[ ! -L "${target_path}" ]]; then
+            echo "Save current ${target_path} to ${target_path}.back"
+            mv "${target_path}" "${target_path}.back"
+        else
+            echo "Remove existing symlink: ${target_path}"
+            rm "${target_path}"
+        fi
     fi
-    
-    echo "create ${HOME}/.config/Code/User/settings.json symlink"
-    ln -s ${PWD}/vscode/settings.json ${HOME}/.config/Code/User/
+
+    echo "# Create symlink: ${source_filepath} > ${target_path}"
+    ln -s ${source_filepath} ${target_folder}/
+}
+
+function restore_vscode {
+    echo "# Create symlink to dotfiles settings"
+    local vscode_settings_folder="$HOME/.config/Code/User"
+    local vscode_settings_filename="settings.json"
+    local vscode_settings_source="${PWD}/vscode/${vscode_settings_filename}"
+    createSymlink ${vscode_settings_folder} ${vscode_settings_filename} ${vscode_settings_source}
+
+    echo "# Create symlink to dotfiles extensions"
+    local vscode_extensions_folder="${vscode_settings_folder}/extensions"
+    local vscode_extensions_filename="extensions.json"
+    local vscode_extensions_source="${PWD}/vscode/${vscode_extensions_filename}"
+    createSymlink ${vscode_extensions_folder} ${vscode_extensions_filename} ${vscode_extensions_source}
+
+    # mkdir -p ${VSCODE_SETTINGS_FOLDER}
+    # if [[ ! -L "${VSCODE_SETTINGS_FILE}" ]]; then
+    #     echo "Save current ${VSCODE_SETTINGS_FILE} to ${VSCODE_SETTINGS_FILE}.back"
+    #     mv "${VSCODE_SETTINGS_FILE}" "${VSCODE_SETTINGS_FILE}.back"
+    # else
+    #     echo "Remove existing symlink"
+    #     rm "${VSCODE_SETTINGS_FILE}"
+    # fi
+
+    # echo("# Create symlink to dotfiles extensions")
+    # echo "create ${VSCODE_SETTINGS_FILE} symlink"
+    # ln -s ${PWD}/vscode/settings.json ${VSCODE_SETTINGS_FOLDER}/
+
+    # VSCODE_EXT_FOLDER=$HOME/.config/Code/User/extensions
+    # VSCODE_EXT_FILE=${VSCODE_EXT_FOLDER}/extensions.json
+    # mkdir -p ${VSCODE_EXT_FOLDER}
+    # if [[ ! -L "${VSCODE_EXT_FILE}" ]]; then
+    #     echo "Save current ${VSCODE_EXT_FILE} to ${VSCODE_EXT_FILE}.back"
+    #     mv "${VSCODE_EXT_FILE}" "${VSCODE_EXT_FILE}.back"
+    # else
+    #     echo "Remove existing symlink"
+    #     rm "${VSCODE_EXT_FILE}"
+    # fi
+
+    # echo "create "${VSCODE_EXT_FILE}" symlink"
+    # ln -s ${PWD}/vscode/extensions.json ${VSCODE_EXT_FOLDER}/
 }
 
 
@@ -130,7 +184,7 @@ function restore_git {
     else
         echo "no existing file ${HOME}/.gitignore_global"
     fi
-    
+
     if [[ -f ${HOME}/.gitconfig ]];then
         echo "backup existing gitconfig"
         mv ${HOME}/.gitconfig ${HOME}/.gitconfig.bak
@@ -140,7 +194,7 @@ function restore_git {
     else
         echo "no existing file ${HOME}/.gitconfig"
     fi
-    
+
     echo "create new symlinks to current dotfiles"
     ln -s ${PWD}/gitfiles/.gitignore_global ${HOME}
     ln -s ${PWD}/gitfiles/.gitconfig ${HOME}
@@ -157,7 +211,7 @@ function restore_docker {
     sudo usermod -aG docker $USER
     echo "check that user is in group"
     groups ${USER}
-    
+
     echo "Log in to the new docker group (to avoid having to log out / log in again)"
     newgrp docker
     sudo su ${USER}
@@ -173,7 +227,7 @@ function restore_docker {
 function restore_system {
     # echo "update arch mirror list"
     # sudo reflector --save /etc/pacman.d/mirrorlist --country France,Germany,Belgium,Netherland --protocol https --latest 10
-    
+
     echo "restore installed packages"
     yay -Syyu
     yay --noconfirm -Sy $(grep -Ev ^'(#|$)' "${PWD}/pacman.txt")
@@ -196,7 +250,7 @@ function restore_starship {
     then
         mkdir -p "${CONFIG_PATH}"
     fi
-    
+
     STARSHIP_CONFIG_FILE="starship.toml"
     if [ -f "${CONFIG_PATH}/${STARSHIP_CONFIG_FILE}" ];
     then
@@ -219,8 +273,8 @@ function restore_starship {
         echo "create new symlink to ${DOTFILES_STARSHIP_CONFIG_FILE_PATH} in ${CONFIG_PATH}"
         ln -s ${DOTFILES_STARSHIP_CONFIG_FILE_PATH} ${CONFIG_PATH}
     fi
-    
-    
+
+
 }
 
 ################
