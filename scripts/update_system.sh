1416166e4fb6291b8c9a533d23f719fd5f6d2c8f
Gabriel Oger <gabriel.oger@gmail.com>
2025-03-25 10:12:41 +0100

    feat: arch update cmd

--- scripts/update_system.sh
+++ scripts/update_system.sh
@@ -1,19 +1,22 @@
 #! /bin/sh
 
 echo "🤝 Update (1/2): Arch mirrors"
-sudo reflector --protocol https --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist --country France,Germany,Netherlands,Switzerland,Finland 
+sudo reflector --protocol https --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist --country France,Germany,Netherlands,Switzerland,Finland
 
 echo "🤝 Update (2/2): EndavourOs mirrors"
-sudo eos-rankmirrors 
+sudo eos-rankmirrors
 
 echo "🔐 Update archlinux-keyring"
-yay -Sy archlinux-keyring 
+yay -Sy archlinux-keyring --noconfirm
 
 echo "🔼 Upgrade system"
 yay -Syu --noconfirm
 
+echo "🔼 Upgrade EndavourOs"
+eos-update
+
 echo "⭐ Clean (1/2): unneeded dependencies"
-yay -Yc
+yay -Scc --noconfirm
 
 echo "⭐ Clean (2/2): journal"
 sudo journalctl --vacuum-time=4weeks
